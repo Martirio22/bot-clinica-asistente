a50443bd915beb46bf44006b2fd809783c136b40
Martin Guerron <guerronmartin@gmail.com>
2026-05-01 18:17:21 -0500

    agregando carpetas a la arquitectura

--- src/lib/Security/Roles/Aplicacion/CrearRole.js
+++ src/lib/Security/Roles/Aplicacion/CrearRole.js
@@ -1,4 +1,4 @@
 const Role = require("../Dominio/Entidades/Role");
 const ConflictError = require("../../../../shared/errors/ConflictError");
-class CrearRole { constructor(roleRepository){this.roleRepository=roleRepository;} async ejecutar(data){const role=new Role(data); if(await this.roleRepository.findByCode(role.code)) throw new ConflictError("Ya existe un rol con ese código"); return await this.roleRepository.create(role);} }
+class CrearRole { constructor(roleRepository) { this.roleRepository = roleRepository; } async ejecutar(data) { const role = new Role(data); if (await this.roleRepository.findByCode(role.code)) throw new ConflictError("Ya existe un rol con ese código"); return await this.roleRepository.create(role); } }
 module.exports = CrearRole;
